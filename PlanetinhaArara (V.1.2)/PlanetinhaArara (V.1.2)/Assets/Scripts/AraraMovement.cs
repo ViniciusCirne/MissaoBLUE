ee75362125640b0b9a90b615a92fa8adb85dd29b
aline e bianca <alinegamagama2018@gmail.com>
2019-04-24 16:21:52 -0300

    primeiro commit
     uhu!

---
+++ PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/AraraMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AraraMovement : MonoBehaviour
+{ 
+	public float speed;
+	private Rigidbody rb;
+	public GameObject gameOver;
+
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		gameOver.SetActive(false);
+		rb = GetComponent<Rigidbody>();
+		speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.CompareTag("Obstacle")) {
+			rb.useGravity = true;
+			gameOver.SetActive(true);
+			speed = 0;
+		}
+	}
+}
---
+++ PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+    public float jump = 3.0f;
+    public float gravity = 9.8f;
+
+    private Vector3 characterMove =  Vector3.zero;
+    
+    private CharacterController ccontroller;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ccontroller = GetComponent<CharacterController>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterMove = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        characterMove = transform.TransformDirection(characterMove);
+        characterMove = characterMove * speed;
+
+//        ccontroller.Move(characterMove * Time.deltaTime);
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+        
+    }
+}

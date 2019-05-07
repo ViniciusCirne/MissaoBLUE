c49141fd3417f2bd9102348193f6779ff65c8a7a
ViniciusMedeirosC <37579933+ViniciusMedeirosC@users.noreply.github.com>
2019-05-07 04:55:13 -0300

    Telas (Menu, Instruções)

---
+++ PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/ButtonScritpt.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScritpt : MonoBehaviour
+{
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void GoToGameplay()
+	{
+		SceneManager.LoadScene("Gameplay");
+	}
+
+	public void GoToInstrucoes()
+	{
+		SceneManager.LoadScene("Instruções");
+	}
+
+	public void GoToConfiguracoes()
+	{
+		SceneManager.LoadScene("Configurações");
+	}
+
+	public void GoToCreditos()
+	{
+		SceneManager.LoadScene("Créditos");
+	}
+
+	public void GoToMenu()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+}

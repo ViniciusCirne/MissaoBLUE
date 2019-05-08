1441c96611dc3ebc22d36956cd2d6f2c2f143047
aline e bianca <alinegamagama2018@gmail.com>
2019-05-08 16:26:24 -0300

    código queimada

---
+++ PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/FireMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FireMovement : MonoBehaviour
+{
+    public int speed; 
+    Rigidbody aaa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aaa = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       aaa.velocity = transform.forward * speed;
+    }
+}

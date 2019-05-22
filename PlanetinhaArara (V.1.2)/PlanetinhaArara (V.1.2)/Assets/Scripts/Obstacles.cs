ae1f923fddfd73e2645a617e81d21d62c5f407ce
TekuIa <rcc.tiago@gmail.com>
2019-05-22 14:21:27 -0300

    coisas
    coiii

---
+++ PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public float fallSpeed = 40.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+    }
+}

b6b9bd420f149d404b4250afbfb1baa5425ba132
ViniciusMedeirosC <37579933+ViniciusMedeirosC@users.noreply.github.com>
2019-05-30 22:34:43 -0300

    coisa tilemap

---
+++ PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+	public GameObject prefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		//prefab.transform.position.x += 3;
+		Instantiate (prefab, new Vector3(prefab.transform.position.x -10, prefab.transform.position.y, prefab.transform.position.z+10), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

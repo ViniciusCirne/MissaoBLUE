5cbb6f0b8c8c25d48e4914b10bbc78f333f2cf8e
José Roberto <robertoescola3@gmail.com>
2019-05-31 12:51:05 -0300

    teste geração procedural

--- PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/SpawnScript.cs
+++ PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/SpawnScript.cs
@@ -7,16 +7,25 @@ public class SpawnScript : MonoBehaviour
 	public GameObject prefab;
 
 
+
     // Start is called before the first frame update
     void Start()
     {
 		//prefab.transform.position.x += 3;
-		Instantiate (prefab, new Vector3(prefab.transform.position.x -10, prefab.transform.position.y, prefab.transform.position.z+10), Quaternion.identity);
+		Instantiate (prefab, new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+              SpawnTrees(); 	//Instantiate (prefab, new Vector3(30, 0, 0), Quaternion.identity);
+        }
+    }
+
+    public void SpawnTrees(Vector3 lastPos)
+    {
+       Instantiate (prefab, new Vector3(lastPos + 30, 0, 0), Quaternion.identity);
     }
 }

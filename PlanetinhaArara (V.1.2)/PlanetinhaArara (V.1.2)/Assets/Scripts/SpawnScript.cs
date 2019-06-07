c71aa9372eaf667a56cc912d99e8bf7da4842005
ViniciusMedeirosC <37579933+ViniciusMedeirosC@users.noreply.github.com>
2019-06-07 10:49:06 -0300

    spawn script principal na cena gameplay

--- PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/SpawnScript.cs
+++ PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/SpawnScript.cs
@@ -9,6 +9,7 @@ public class SpawnScript : MonoBehaviour
 	public float arraySize;
 	public int xPos;
 	public int yPos;
+	public bool canSpawn;
    
 
     Vector3 lastPosi;
@@ -16,17 +17,24 @@ public class SpawnScript : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        lastPosi = new Vector3(xPos, yPos, 0);
+	{   StartCoroutine(TempoDeVitoria());
+		canSpawn = true;
+        lastPosi = new Vector3(xPos, yPos, playerPosi.position.z + 35.0f);
         nextPosi = new Vector3(xPos, yPos, 0);
+
+		for (int i = 0; i < 3; i++)
+		{
+			SpawnTrees(nextPosi, lastPosi);
+		}
+
 		//prefab.transform.position.x += 3;
-		Instantiate (prefab[0], lastPosi, Quaternion.identity);
+		//Instantiate (prefab[0], lastPosi, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-		if(playerPosi.position.z >= lastPosi.z - 20) //Input.GetButtonDown("Fire1") playerPosi.position.z >= lastPosi.z + 5
+		if(playerPosi.position.z >= lastPosi.z - 40 && canSpawn) //Input.GetButtonDown("Fire1") playerPosi.position.z >= lastPosi.z + 5
         {
             for (int i = 0; i < 3; i++)
             {
@@ -35,20 +43,26 @@ public class SpawnScript : MonoBehaviour
         
                	//Instantiate (prefab, new Vector3(30, 0, 0), Quaternion.identity);
         }
+		else if (canSpawn == false)
+		{
+			Debug.Log("parabens vc ganhou");
+		}
     }
 
     void SpawnTrees(Vector3 nextPosiii, Vector3 lastPosiii)
     {
-       
-
         float randomNum = (Random.Range (0.0f, arraySize));
 		Debug.Log(Mathf.RoundToInt(randomNum));
         nextPosiii = new Vector3 (xPos, yPos, lastPosiii.z + 30);
         lastPosi = nextPosiii;
-		Instantiate (prefab[Mathf.RoundToInt(randomNum)], nextPosiii, Quaternion.identity);
-       
-	   
+		Instantiate (prefab[Mathf.RoundToInt(randomNum)], nextPosiii, Quaternion.identity);  
     }
+
+	IEnumerator TempoDeVitoria()
+	{
+		yield return new WaitForSeconds(60);
+		canSpawn = false;
+	}
        
     
 }

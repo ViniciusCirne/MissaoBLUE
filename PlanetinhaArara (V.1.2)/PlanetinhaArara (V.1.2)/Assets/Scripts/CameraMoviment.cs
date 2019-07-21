8b068cb42b875c57adc03ea80847a59dfa545fec
Marília Costa <marilia.gouveia.costa@gmail.com>
2019-07-21 14:35:52 -0300

    Movimento da Câmera OK

---
+++ PlanetinhaArara (V.1.2)/PlanetinhaArara (V.1.2)/Assets/Scripts/CameraMoviment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoviment : MonoBehaviour
+{
+    public Transform target;
+    
+    void Update()
+    {
+        
+        transform.position = target.position;
+
+    }
+}
